2a7b1a43258d06e0ff4460e0f713ceeaa600353d
Jiahui Xu <jxu@ethz.ch>
2025-01-28 17:26:56 +0100

    Remove exception

--- src/exceptions.py
+++
@@ -1,6 +0,0 @@
-from src.utils import COLOR_RED, COLOR_NC
-
-class Dot2SmvNotImplementedError(NotImplementedError):
-    def __init__(self, message):            
-        # Call the base class constructor with the parameters it needs
-        super().__init__(COLOR_RED + message + COLOR_NC)
